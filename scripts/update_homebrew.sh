ecf5d4dcc207cded0afd2ff23ebb049c83b6bdde
Tamimi <tamimi@solace.com>
2024-09-06 08:06:10 -0700

    update

--- scripts/update_homebrew.sh
+++ scripts/update_homebrew.sh
@@ -6,19 +6,17 @@ REPO_NAME="SolaceLabs/solace-tryme-cli"
 TAP_NAME="SolaceLabs/homebrew-stm.git"
 FORMULA_NAME="stm"
 
-echo "In update homebrew script"
 # Clone the Homebrew tap
 git clone https://github.com/$TAP_NAME
 cd homebrew-$FORMULA_NAME
-echo "in homebrew-$FORMULA_NAME"
 
 # Update formula
 pkg_version=$1
 echo "https://github.com/$REPO_NAME/releases/download/$pkg_version/stm-macos-v$pkg_version.zip"
 echo "Formula/$FORMULA_NAME.rb"
-sed -i '' "s|url .*|url \"https://github.com/$REPO_NAME/releases/download/$pkg_version/stm-macos-v$pkg_version.zip\"|" Formula/$FORMULA_NAME.rb
+sed -i "s|url .*|url \"https://github.com/$REPO_NAME/releases/download/$pkg_version/stm-macos-v$pkg_version.zip\"|" Formula/$FORMULA_NAME.rb
 pwd
-sed -i '' "s|sha256 .*|sha256 \"$(shasum -a 256 ../release/stm-macos-v$pkg_version.zip | awk '{ print $1 }')\"|" Formula/$FORMULA_NAME.rb
+sed -i "s|sha256 .*|sha256 \"$(shasum -a 256 ../release/stm-macos-v$pkg_version.zip | awk '{ print $1 }')\"|" Formula/$FORMULA_NAME.rb
 
 # Commit and push changes
 echo "Committing and pushing changes... $pkg_version"
