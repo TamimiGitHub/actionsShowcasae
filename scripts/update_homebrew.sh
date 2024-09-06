f6d0d6ff6903467533c48b079d3844635efad702
Tamimi <tamimi@solace.com>
2024-09-06 08:10:55 -0700

    bug_fix

--- scripts/update_homebrew.sh
+++ scripts/update_homebrew.sh
@@ -12,14 +12,13 @@ cd homebrew-$FORMULA_NAME
 
 # Update formula
 pkg_version=$1
-echo "https://github.com/$REPO_NAME/releases/download/$pkg_version/stm-macos-v$pkg_version.zip"
-echo "Formula/$FORMULA_NAME.rb"
 sed -i "s|url .*|url \"https://github.com/$REPO_NAME/releases/download/$pkg_version/stm-macos-v$pkg_version.zip\"|" Formula/$FORMULA_NAME.rb
-pwd
 sed -i "s|sha256 .*|sha256 \"$(shasum -a 256 ../release/stm-macos-v$pkg_version.zip | awk '{ print $1 }')\"|" Formula/$FORMULA_NAME.rb
 
 # Commit and push changes
 echo "Committing and pushing changes... $pkg_version"
+git config --global user.email "community@solace.com"
+git config --global user.name "SollyBot"
 # git commit -am "Update $FORMULA_NAME to $pkg_version"
 # git push
 
