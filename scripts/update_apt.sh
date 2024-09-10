4dd365f752bc78bac9026db7249f5fc1e909bd30
Tamimi <tamimi@solace.com>
2024-09-10 11:12:22 -0700

    new

---
+++ scripts/update_apt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Install required package
+sudo apt install -y reprepro
+
+# Define variables
+APT_SERVER="SolaceLabs/apt-stm.git"
+pkg_version=$1
+
+# Clone the APT Server
+git clone https://x-access-token:$GITHUB_TOKEN@github.com/$APT_SERVER
+
+# Prepare for APT update
+reprepro -b apt-stm/ --priority 0 includedeb stm ../releases/stm-linux_$pkg_version_amd64
+
+#### TEST ####
+ls -lR apt-stm/
+#### TEST ####
+
+
+# Commit and push changes
+# cd apt-stm/
+# git config --global user.email "community@solace.com"
+# git config --global user.name "SollyBot"
+# git commit -am "Update stm to v$pkg_version"
+# git push
